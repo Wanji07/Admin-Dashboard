d37458174e03ba8c288da4638318ccaaded7771f
William <ewilliammiguel@gmail.com>
2024-12-15 20:39:51 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const contentTrending = document.querySelectorAll('.content-trending');
 const projectCard = document.querySelectorAll('.project-card');  
 const favBtn = document.getElementById('fav-btn');
 const favIcon = document.getElementById('fav-icon');
+const missingCard = document.querySelectorAll('#missing-project-card');
 
 let toggled = false;
 
@@ -15,12 +16,14 @@ toggleBtn.addEventListener('click', () => {
     toggled = !toggled;
     if (toggled == true) {
         document.body.style.color = 'black';
+        document.body.style.backgroundColor = 'rgb(238, 238, 238)';
         navigation.classList.add('light-mode-nav');
         section.classList.add('light-mode-section');
         aside.classList.add('light-mode-aside');
         announcement.forEach(announcement => announcement.classList.add('light-mode-card'));
         contentTrending.forEach(trend => trend.classList.add('light-mode-card'));
         projectCard.forEach(card => card.classList.add('light-mode-card'));
+        missingCard.forEach(card => card.classList.add('light-mode-missing-card'));
         toggleIcon.classList.remove('fa-sun');
         toggleIcon.classList.add('fa-moon');
     } else {
@@ -30,6 +33,7 @@ toggleBtn.addEventListener('click', () => {
         announcement.forEach(announcement => announcement.classList.remove('light-mode-card'));
         contentTrending.forEach(trend => trend.classList.remove('light-mode-card'));
         projectCard.forEach(card => card.classList.remove('light-mode-card'));
+        missingCard.classList.remove('light-mode-missing-card');
         toggleIcon.classList.add('fa-sun');
         toggleIcon.classList.remove('fa-moon');
     }
